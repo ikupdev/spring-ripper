1cbe17ef0b4ba5cd84feb63e9ef5ff9d5baafef0
Ilya Kupriyanov <ikupdev@gmail.com>
2022-03-10 12:48:17 +0300

    InjectRandomIntAnnotationBeanPostProcessor

---
+++ src/main/java/ru/kuper/InjectRandomInt.java
@@ -0,0 +1,14 @@
+package ru.kuper;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Ilya V. Kupriyanov
+ * @version 10.03.2022
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectRandomInt {
+    int min();
+    int max();
+}
--- src/main/java/ru/kuper/TerminatorQuoter.java
+++ src/main/java/ru/kuper/TerminatorQuoter.java
@@ -5,14 +5,24 @@ package ru.kuper;
  * @version 10.03.2022
  */
 public class TerminatorQuoter implements Quoter {
+
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     private String message;
+
+    public TerminatorQuoter() {
+        System.out.println(repeat);
+    }
 
     public void setMessage(String message) {
         this.message = message;
     }
 
     public void sayQuote() {
-        System.out.println(message);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(message);
+        }
     }
 
 }
